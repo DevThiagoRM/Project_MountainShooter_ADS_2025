39d39e496aca46f2cb89009da7a5284659d26f96
Thiago Recetto Moraes <thiago.social@outlook.com>
2025-03-26 20:49:14 -0300

    Adding classes generated from starUML

---
+++ code/entity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.Attribute2 = None
+        self.Attribute3 = None
+        self.Attribute4 = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
+    def Operation2(self, ):
+        pass
+
+    def Operation3(self, ):
+        pass
