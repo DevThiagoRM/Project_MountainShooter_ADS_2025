e2fe8ee7b23afb52ab4be845fb2a41096e04a751
Thiago Recetto Moraes <thiago.social@outlook.com>
2025-03-27 11:04:46 -0300

    Creating Players and Enemies Moving players and Enemies

--- code/level.py
+++ code/level.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 import sys
+from random import choice
 
 import pygame
 from typing import List
@@ -8,7 +10,7 @@ from typing import List
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import COLOR_WHITE, WIN_HEIGHT, WIN_WIDTH
+from code.Const import COLOR_WHITE, WIN_HEIGHT, WIN_WIDTH, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 
@@ -21,6 +23,10 @@ class Level:
         self.game_Mode = game_mode
         self.entity_list: List[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME, 0)
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple, ):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans TypeWriter", size=text_size)
@@ -49,6 +55,10 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             # Printed text
             self.level_text(14, f'{self.name} Timeout: {self.timeout / 1000:.1f}s', COLOR_WHITE, (10,5))
--- code/player.py
+++ code/player.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.key
 
-from Entity import Entity
+from code.Const import WIN_HEIGHT, WIN_WIDTH, ENTITY_SPEED, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
+    PLAYER_KEY_RIGHT
+from code.entity import Entity
 
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
-    def move(self, ):
-        pass
+    def move(self):
+        pressed_key = pygame.key.get_pressed()
+        movement = [0, 0]  # [x, y]
+
+        # PLAYERS MOVE
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0: # MOVE UP
+            movement[1] -= ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT: # MOVE DOWN
+            movement[1] += ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0: # MOVE LEFT
+            movement[0] -= ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH: # MOVE RIGHT
+            movement[0] += ENTITY_SPEED[self.name]
+
+        self.rect.move_ip(movement)
