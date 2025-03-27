861904cdc8f33821976f42e5f01095253e69ab07
Thiago Recetto Moraes <thiago.social@outlook.com>
2025-03-26 21:46:11 -0300

    Creating assets folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,17 @@
+# Constants screen image
+
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+              'NEW GAME 2P - COOPERATIVE',
+              'NEW GAME 2P - COMPETITIVE',
+              'SCORE',
+              'EXIT')
+
+# W
+WIN_WIDTH = 576 # Width
+WIN_HEIGHT = 324 # Height
